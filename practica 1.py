c1e79a8e3db8974c91bae35ed86082f9ff9587e8
HugoLozanoSQ9 <hugolozano19@aragon.unam.mx>
2024-02-15 19:36:15 -0600

    Practicas de la 1 a la 6 con modificación en practica 1

--- practica 1.py
+++ practica 1.py
@@ -1,4 +1,5 @@
-#Escribir un programa que imprima la vibora de la mar
+#Escribir un programa que imprima la vibora de la mar, si solamente se escriben 3 comillas entonces no estáría ocupando un lugar
+#En memoria
 vivora = """"A la víbora, víbora 
     De la mar, de la mar
         Por aquí pueden pasar
